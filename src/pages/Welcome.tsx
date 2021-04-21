02679ac1217e83f9238a6d8c6b831884330f77cd
Lucas Cruz <lucascruz2773@gmail.com>
2021-04-21 17:22:11 -0300

    feat: Landing page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,35 +1,49 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+import { 
+    SafeAreaView, 
+    Text, 
+    View,
+    Image, 
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native'
 
-import { Button } from '../components/Button'
+import Feather from 'react-native-vector-icons/Feather';
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
 
 export function Welcome() {
-  
-  const [visible, setVisible] = useState(false);
 
-  function handleVisibility() {
-    setVisible(!visible);
-  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
-      {
-          visible &&
-          <Image source={wateringImg} style={styles.image} />
-      }
-      <Text style={styles.subtitle}>
-          Não esqueça mais de regar suas plantas. 
-          Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
-      <Button title=">" />
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+            Gerencie {'\n'}
+            suas plantas de{'\n'}
+            forma fácil
+        </Text>
+            <Image 
+                source={wateringImg} 
+                style={styles.image} 
+                resizeMode="contain"
+            />
+        <Text style={styles.subtitle}>
+            Não esqueça mais de regar suas plantas. 
+            Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+        <TouchableOpacity 
+            style={styles.button} 
+            activeOpacity={0.7}
+        >
+            <Feather
+                name="chevron-right"  
+                style={styles.buttonIcon}
+            />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -38,24 +52,42 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#FFF',
     flex: 1, 
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-      fontSize: 32,
+      fontSize: 28,
       fontWeight: 'bold',
       textAlign: 'center',
       color: colors.heading,
       marginTop: 38,
+      lineHeight: 34
   },
   subtitle: {
       textAlign: 'center',
       fontSize: 18,
       paddingHorizontal: 20,
       color: colors.heading,
+      lineHeight: 28,
   },
   image: {
-      width: 292,
-      height: 284
+    height: Dimensions.get('window').width * 0.7,
   },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
+  }
 })
